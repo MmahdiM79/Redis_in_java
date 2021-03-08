530be4ea3097c8f36613337d517c700106e4dbdc
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-03-08 15:40:14 +0330

    redis client changed

--- project_0/Main.java
+++ project_0/Main.java
@@ -1,9 +1,4 @@
-import io.lettuce.core.*;
-import io.lettuce.core.api.*;
-import io.lettuce.core.api.sync.*;
-
-
-
+import redis.clients.jedis.Jedis;
 
 
 
@@ -12,12 +7,11 @@ public class Main
 
             /*  Fields  */
 
+    // Data Base
+    private static Jedis db = new Jedis("localhost");
 
-    // Data Base part
-    private static RedisClient db = RedisClient.create("redis://localhost:6379/0");
-    private static StatefulRedisConnection<String, String> connection = db.connect();
-    private static RedisCommands<String, String> syncCommands = connection.sync();
 
+    
     public static void main(String[] args) 
     {
         
